73fb943fb8f8bca5e1f7a567387f6869202514c9
WilsonLe <leanhminh2907@gmail.com>
2023-05-05 21:01:55 -0400

    initial commit, boilerplate for nextjs and payloadcms

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const path = require("path");
+const { withPayload } = require("@payloadcms/next-payload");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true
+};
 
-module.exports = nextConfig
+const payloadConfig = {
+	// The second argument to `withPayload`
+	// allows you to specify paths to your Payload dependencies.
+
+	// Point to your Payload config (Required)
+	configPath: path.resolve(__dirname, "./src/payload/payload.config.ts"),
+
+	// Point to your exported, initialized Payload instance (optional, default shown below`)
+	payloadPath: path.resolve(process.cwd(), "./src/payload/payloadClient.ts")
+};
+
+module.exports = withPayload(nextConfig, payloadConfig);
